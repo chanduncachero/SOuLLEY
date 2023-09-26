f2b111a9dc355f8d736272a723e75fed6d93643f
chandun <chanduncachero@gmail.com>
2023-09-27 05:02:33 +0800

    Socket io CORS Error 5th solution

--- App.js
+++ App.js
@@ -20,7 +20,7 @@ const { exit } = require("process");
 const { error } = require("console");
 // const Chat = require("twilio/lib/rest/Chat");
 
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 7700; 
 const app = express();
 
 const http = require("http");
