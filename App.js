aa977003c0b581b8e32d3c0df0305b6db200c7b3
chandun <chanduncachero@gmail.com>
2023-09-27 01:46:57 +0800

    Socket io CORS Error 5th solution

--- App.js
+++ App.js
@@ -46,7 +46,7 @@ const io = require('socket.io')(server, {
 });
 
 //CORS SOLUTION
-// const cors = require('cors')
+const cors = require('cors');
 // const corsOptions ={
 //     origin:'http://localhost:3000', 
 //     credentials:true,            //access-control-allow-credentials:true
@@ -121,7 +121,7 @@ const wrap = middleware => (socket, next) =>
 app.use(sessionMiddleware);
 
 io.engine.use(sessionMiddleware);
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));   
 app.use(express.static(__dirname+"/public"));
